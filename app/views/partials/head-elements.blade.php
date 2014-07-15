c74f39c50983fdcd522e3dfa698abf826cc77f9d
Doug Niccum <dniccum@ingenology.com>
2014-07-15 16:40:32 -0500

    Broke out a few parts of the master template to allow for customization of the homepage

---
+++ app/views/partials/head-elements.blade.php
@@ -0,0 +1,15 @@
+<meta name="viewport" content="width=device-width">
+<link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
+<link rel="apple-touch-icon" href="{{ asset('images/apple-touch-icon.png') }}" />
+<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('images/apple-touch-icon-57x57.png') }}" />
+<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('images/apple-touch-icon-72x72.png') }}" />
+<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('images/apple-touch-icon-76x76.png') }}" />
+<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('images/apple-touch-icon-114x114.png') }}" />
+<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('images/apple-touch-icon-120x120.png') }}" />
+<link rel="apple-touch-icon" sizes="144x144" href="{{ asset('images/apple-touch-icon-144x144.png') }}" />
+<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('images/apple-touch-icon-152x152.png') }}" />
+
+<link rel="stylesheet" href="{{ asset('styles/vendor.css') }}">
+<link rel="stylesheet" href="{{ asset('styles/styles.css') }}">
+
+<script src="{{ asset('scripts/vendor/modernizr.js') }}"></script>
